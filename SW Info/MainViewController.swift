074ae44abf860c58915d3643946e04ed85cde9b4
Donny Davis <donnywdavis@icloud.com>
2016-05-22 09:35:33 -0400

    Start over with static table cells for the main view

--- SW Info/MainViewController.swift
+++ SW Info/MainViewController.swift
@@ -8,51 +8,19 @@
 
 import UIKit
 
-class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    // MARK: IBOutlets
-    
-    @IBOutlet weak var tableView: UITableView!
-    
-    // MARK: Properties
-    
-    let endpoints = ["Films", "Characters", "Species"]
+class MainViewController: UIViewController {
 
     
     // MARK: View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Clear any unpopulated cells from displaying
-        tableView.tableFooterView = UIView(frame: CGRectZero)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    // MARK: Table View Delegates
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return endpoints.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCellWithIdentifier("EndpointCell", forIndexPath: indexPath) as? MainTableViewCell else {
-            let alert = UIAlertController(title: "Error", message: "Error loading table cell", preferredStyle: .Alert)
-            let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
-            alert.addAction(okAction)
-            presentViewController(alert, animated: true, completion: nil)
-            return UITableViewCell()
-        }
-        
-        cell.setUpCell(endpoints[indexPath.row])
-        
-        return cell
-    }
-
 
 }
 
